a4b9004933033792d3a0ec5955d07cf903d39ad0
dinushchathurya <dinushchathurya21@gmail.com>
2022-06-01 20:31:29 +0530

    feat: Add global varibales

--- main.tf
+++ main.tf
@@ -8,8 +8,10 @@ terraform {
 }
 
 provider "aws" {
-    region  = "${var.aws_region}"
-    profile = "${var.aws_profile}"
+	version = "~> 2.0"
+	access_key = var.access_key 
+	secret_key = var.secret_key 
+	region     = var.region
 }
 
 resource "aws_vpc" "vpc" {
