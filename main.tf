4dcd589439627d9f4cdafb5194838827135176df
dinushchathurya <dinushchathurya21@gmail.com>
2022-06-01 20:21:51 +0530

    feat: Create sample vpc

--- main.tf
+++ main.tf
@@ -1,22 +1,21 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "3.74.0"
+    required_providers {
+        aws = {
+            source  = "hashicorp/aws"
+            version = "3.74.0"
+        } 
     }
-  }
 }
 
 provider "aws" {
-  region  = "${var.aws_region}"
-  profile = "${var.aws_profile}"
+    region  = "${var.aws_region}"
+    profile = "${var.aws_profile}"
 }
 
 resource "aws_vpc" "main" {
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "default"
-
-  tags = {
-    Name = "main"
-  }
+    cidr_block       = "10.0.0.0/16"
+    
+    tags = {
+      Name = "main"
+    }
 }
