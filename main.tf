1f190fdabce3b2f79a0d3b9d0390681e25518f40
dinushchathurya <dinushchathurya21@gmail.com>
2022-06-01 20:23:40 +0530

    feat: Create sample vpc

--- main.tf
+++ main.tf
@@ -12,10 +12,15 @@ provider "aws" {
     profile = "${var.aws_profile}"
 }
 
-resource "aws_vpc" "main" {
-    cidr_block       = "10.0.0.0/16"
-    
+resource "aws_vpc" "vpc" {
+    cidr_block           = "10.0.0.0/16"
+    enable_dns_support   = "true"
+    enable_dns_hostnames = "true"
+    enable_classiclink   = "false"
+    instance_tenancy     = "default"
+
     tags = {
-      Name = "main"
+        Name = "vpc"
     }
 }
+
