e88d68c94942687629acc63bc5ff582f42415251
dinushchathurya <dinushchathurya21@gmail.com>
2022-06-03 21:06:27 +0530

    config: Add AWS Credentials configuration

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
-terraform {
-    required_providers {
-        aws = {
-            source  = "hashicorp/aws"
-            version = "3.74.0"
-        } 
-    }
-}
+# terraform {
+#     required_providers {
+#         aws = {
+#             source  = "hashicorp/aws"
+#             version = "3.74.0"
+#         } 
+#     }
+# }
 
 provider "aws" {
 	access_key = var.access_key 
