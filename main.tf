1e47db71df010da14a24d8f4d6f86f2cdf88858b
dinushchathurya <dinushchathurya21@gmail.com>
2022-06-11 21:00:03 +0530

    refactor: Change AWS provider version

--- main.tf
+++ main.tf
@@ -7,11 +7,11 @@ terraform {
     }
 }
 
-provider "aws" {
-	access_key = var.access_key 
-	secret_key = var.secret_key 
-	region     = var.aws_region
-}
+# provider "aws" {
+# 	access_key = var.access_key 
+# 	secret_key = var.secret_key 
+# 	region     = var.aws_region
+# }
 
 resource "aws_instance" "example" {
     ami           = "ami-2757f631"
